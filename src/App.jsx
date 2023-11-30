3cf6010def15b9856a5002052ba681814874d25c
James Alderman <80925002+JAldo1980@users.noreply.github.com>
2023-11-30 12:25:41 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import "./App.css";
-import Header from "/components/Header.jsx";
-import About from "/components/About.jsx";
-import Work from "/components/Work.jsx";
-import Wordpress from "/components/Wordpress.jsx";
-import Footer from "/components/Footer.jsx";
-import Contact from "/components/Contact.jsx";
+import Header from "../components/Header.jsx";
+import About from "../components/About.jsx";
+import Work from "../components/Work.jsx";
+import Wordpress from "../components/Wordpress.jsx";
+import Footer from "../components/Footer.jsx";
+import Contact from "../components/Contact.jsx";
 
 export default function App() {
   return (
