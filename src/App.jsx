fda6959ca8421d67d6a47fb282b9d24b6768df46
JAldo1980 <jamesalderman1980@gmail.com>
2023-11-30 14:07:50 +0000

    comments added

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Wordpress from "../components/Wordpress.jsx";
 import Footer from "../components/Footer.jsx";
 import Contact from "../components/Contact.jsx";
 
-export default function App() {
+export default function App() {}
   return (
     <>
       <Header />
