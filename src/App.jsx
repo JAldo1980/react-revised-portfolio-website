30a0e0aa65d00dbb2ac8f117d668c1fb2dd7d5c2
JAldo1980 <jamesalderman1980@gmail.com>
2023-11-30 14:12:26 +0000

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React from "react";
 import "./App.css";
 import Header from "../components/Header.jsx";
 import About from "../components/About.jsx";
@@ -7,7 +7,7 @@ import Wordpress from "../components/Wordpress.jsx";
 import Footer from "../components/Footer.jsx";
 import Contact from "../components/Contact.jsx";
 
-export default function App() {}
+export default function App() {
   return (
     <>
       <Header />
