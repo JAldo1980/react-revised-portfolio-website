e7d0dc74d829d98f47896a60d60c3cf90ab651a1
JAldo1980 <jamesalderman1980@gmail.com>
2024-01-07 13:23:42 +0000

    updated About section

--- components/About.jsx
+++ components/About.jsx
@@ -5,43 +5,52 @@ export default function About() {
     <div className="about-container" id="about-scroll">
       <div className="about-inner-container">
         <h2>About</h2>
-        <p>Hi, welcome to my portfolio 😊.</p>
-        <br></br>
         <p>
-          Current tech used: <strong>Node, React, JavaScript, CSS, HTML</strong>
+          Hi, Welcome to my portfolio, where my focus is on delivering tangible
+          value to your organisation.
         </p>
         <br></br>
         <p>
-          In my ongoing professional development, I am actively honing my
-          expertise in Node, React, JavaScript, CSS, and HTML. My primary
-          objective is to advance towards proficiently crafting MERN stack
-          applications, ensuring a robust capability to provide effective
-          solutions for diverse business challenges.
+          Current tech used:{" "}
+          <strong>Node, React, JavaScript, CSS, HTML, WordPress</strong>
         </p>
         <br></br>
         <p>
-          My initiation into web development traces back to my university years,
-          during which I constructed blogs using WordPress.com. This initial
-          exposure evolved into hands-on experience with various Content
-          Management Systems, including Squarespace and WIX. Intrigued by the
-          underlying technologies powering these CMS platforms and their
-          intricate functionalities, I recognised the transformative potential
-          of web development and the broader tech industry.
+          In my continual pursuit of professional development, I actively refine
+          my skills in Node, React, JavaScript, CSS, and HTML. My core objective
+          early this year is to craft MERN stack applications, equipping me with
+          the capability to provide tailored solutions that address your
+          specific business challenges.
         </p>
         <br></br>
         <p>
-          This realisation became particularly evident during my tenure as the
-          founder of a Limited company specialising in digital recruitment.
-          Observing firsthand the profound impact of web development and
-          technology on business operations fuelled my determination to
-          transition into a front-end web developer.
+          In addition to my technical knwoledge, I bring a wealth of expertise
+          as a qualified and seasoned digital marketer, presenting a valuable
+          dual skill set. My commitment is to leverage these skills to enhance
+          your business objectives, whether it's through creating engaging user
+          interfaces or implementing effective digital marketing strategies. I'm
+          enthusiastic about the opportunity to contribute to your team's
+          success and appreciate your time in considering how my journey aligns
+          with your organisation's growth.
         </p>
         <br></br>
         <p>
-          Complementing my technical skills, I bring a wealth of expertise as a
-          qualified and seasoned digital marketer, adding a valuable dimension
-          to my professional profile. I appreciate your time in considering my
-          journey and aspirations. Thank you for reading! 😊
+          My journey into web development began during my university years,
+          where I constructed blogs using WordPress.com. This initial experience
+          evolved into hands-on proficiency with various Content Management
+          Systems like Squarespace and WIX. Fueled by my fascination with the
+          technologies powering these platforms and their intricate
+          functionalities, I recognised the transformative potential of web
+          development within the broader tech industry.
+        </p>
+        <br></br>
+        <p>
+          This awareness deepened during my role as the founder of a Limited
+          company specialising in digital recruitment. Witnessing firsthand the
+          profound impact of web development and technology on optimising
+          business operations ignited my determination to transition into a
+          front-end web developer (with additional value adds such as digital
+          marketing expereince). Thank you for reading 😊
         </p>
       </div>
     </div>
