7cd76aa3d9f9effc5c34ee4fe57a401dce1d3b86
James Alderman <80925002+JAldo1980@users.noreply.github.com>
2024-03-09 10:38:00 +0000

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className="footer-el">
       <small>
-        © 2023 - Created by James Alderman. <br></br>All Rights Reserved.
+        © 2024 - Created by James Alderman. <br></br>All Rights Reserved.
       </small>
     </div>
   );
